ecb6e56f619442598159d64fa9c249f0a752ff30
Matt Bianco <matthewbianco@protonmail.com>
2021-04-03 12:06:50 -0400

    added 'add employee and role, tho not working'

--- index.js
+++ index.js
@@ -131,7 +131,29 @@ allRoles = () => {
     })
   }      
 
-//    addRole();
+  function addRole() {
+    inquirer.prompt([
+        {
+            message: "enter title:",
+            type: "input",
+            name: "title"
+        }, {
+            message: "enter salary:",
+            type: "number",
+            name: "salary"
+        }, {
+            message: "enter department ID:",
+            type: "number",
+            name: "department_id"
+        }
+    ]).then(function (response) {
+        connection.query("INSERT INTO role (title, salary, department_id) values (?, ?, ?)", [response.title, response.salary, response.department_id], function (err, data) {
+            console.table(data);
+        })
+        askQuestions();
+    })
+  
+  }
 
 //     addEmployee();
 
